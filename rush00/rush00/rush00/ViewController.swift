1521af6d0ce8ce1e622adf1963b1bd7ba257450f
Michael Brave <mbrave@e1z1r8p22.42.us.org>
2019-10-07 11:19:01 -0700

    halfway there but not crashing

--- rush00/rush00/rush00/ViewController.swift
+++ rush00/rush00/rush00/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar:UISearchBar!
+    @IBOutlet weak var tblView: UITableView!
+    
+    let movieName = ["joker", "snatch", "kung fu hustle"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -45,5 +50,27 @@ class ViewController: UIViewController {
     @IBAction func test_btn(_ sender: Any) {
         getData()
     }
+}
+
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = movieName[indexPath.row]
+        return cell!
+    }
     
 }
+
+
+
+
+
+
+
+
+
